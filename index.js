feec9e709fb156d0f0d5ecf3cd7d6e6cc327133e
jeevankc17 <jkc5186@gmail.com>
2022-06-08 15:41:30 +0545

    Adding files for first time

---
+++ index.js
@@ -0,0 +1,37 @@
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+var account;
+web3.eth.getAccounts().then((f) => {
+ account = f[0];
+})
+
+abi = JSON.parse('[{"inputs":[{"internalType":"bytes32[]","name":"candidateNames","type":"bytes32[]"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"candidateList","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"}]')
+
+contract = new web3.eth.Contract(abi);
+contract.options.address = "0x33cd30a07866EAa3D3775e5Fc0e54E0A36b3643F";
+// update this contract address with your contract address
+
+candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
+
+function voteForCandidate(candidate) {
+ candidateName = $("#candidate").val();
+ console.log(candidateName);
+
+ contract.methods.voteForCandidate(web3.utils.asciiToHex(candidateName)).send({from: account}).then((f) => {
+  let div_id = candidates[candidateName];
+  contract.methods.totalVotesFor(web3.utils.asciiToHex(candidateName)).call().then((f) => {
+   $("#" + div_id).html(f);
+  })
+ })
+}
+
+$(document).ready(function() {
+ candidateNames = Object.keys(candidates);
+
+ for(var i=0; i<candidateNames.length; i++) {
+ let name = candidateNames[i];
+  
+ contract.methods.totalVotesFor(web3.utils.asciiToHex(name)).call().then((f) => {
+  $("#" + candidates[name]).html(f);
+ })
+ }
+});
